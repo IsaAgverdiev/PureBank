61913e8bfd492632b967d73d8ab103510abc5f49
Isa <74831265+IsaAgverdiev@users.noreply.github.com>
2021-05-02 22:35:49 +0300

    add QA block

--- app/js/main.js
+++ app/js/main.js
@@ -2,3 +2,15 @@ VanillaTilt.init(document.querySelector(".card-item"), {
 	max: 20,
 	speed: 100
 });
+
+function showAccordion() {
+	const accordionHeader = document.querySelectorAll('.qa__accordion-header');
+	for (i = 0; i < accordionHeader.length; i++) {
+		accordionHeader[i].addEventListener('click', function () {
+			this.nextElementSibling.classList.toggle('qa__accordion-body--active')
+			this.children[1].classList.toggle('qa__accordion-icon--minus')
+		})
+	}
+}
+
+showAccordion();

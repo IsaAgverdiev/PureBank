debed55f1da246f00969571a663888c21efef1b4
Isa <74831265+IsaAgverdiev@users.noreply.github.com>
2021-05-13 13:24:58 +0300

    valided html form

--- app/js/main.js
+++ app/js/main.js
@@ -72,3 +72,94 @@ function select() {
 inputRub.oninput = () => {
   inputOther.value = (parseFloat(inputRub.value) / rates.USD.Value).toFixed(2);
 };
+
+function labelUp() {
+  let input = document.querySelectorAll(".form__input");
+  let label = document.querySelectorAll(".form__label");
+
+  for (let i = 0; i < input.length; i++) {
+    const inputReq = input[i];
+    let labelReq = inputReq.nextElementSibling;
+
+    inputReq.oninput = () => {
+      if (inputReq.value !== "") {
+        labelReq.style.fontSize = "11px";
+        labelReq.style.color = "#CA48F6";
+        labelReq.style.top = "14px";
+      } else {
+        labelReq.style.fontSize = "18px";
+        labelReq.style.color = "#302d42";
+        labelReq.style.top = "50%";
+        inputReq.blur();
+      }
+    };
+  }
+}
+labelUp();
+
+let form = document.querySelector(".form");
+form.addEventListener("submit", formSend);
+
+async function formSend(e) {
+  e.preventDefault();
+
+  let error = formValidate(form);
+
+  let formData = new FormData(form);
+
+  if (error === 0) {
+    form.classList.add("_sending");
+    let response = await fetch("sendmail.php", {
+      method: "POST",
+      body: formData,
+    });
+    if (response.ok) {
+      let result = await response.json();
+      alert(result.message);
+      form.reset();
+    } else {
+      alert("Ошибка");
+    }
+  } else {
+    alert("Заполните обязательные поля");
+  }
+}
+
+function formValidate(form) {
+  let error = 0;
+  let formReq = document.querySelectorAll("._req");
+  let label = document.querySelectorAll(".form__label");
+  for (let i = 0; i < formReq.length; i++) {
+    const input = formReq[i];
+    formRemoveError(input);
+
+    if (input.classList.contains("_email")) {
+      if (emailTest(input)) {
+        formAddError(input);
+        error++;
+      }
+    } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+      formAddError(input);
+      error++;
+    } else {
+      if (input.value === "") {
+        formAddError(input);
+        error++;
+      }
+    }
+  }
+  return error;
+}
+
+function formAddError(input) {
+  input.parentElement.classList.add("_error");
+  input.classList.add("_error");
+}
+
+function formRemoveError(input) {
+  input.parentElement.classList.remove("_error");
+  input.classList.remove("_error");
+}
+function emailTest(input) {
+  return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
